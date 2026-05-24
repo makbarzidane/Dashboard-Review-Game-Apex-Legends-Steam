4fdb8109a0a679b051e6f7e068d8893b98b8a2d6
makbarzidane <akbarzidane12@gmail.com>
2026-05-24 11:01:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,7 @@ from wordcloud import WordCloud
 # KONFIGURASI HALAMAN
 # =========================================================
 st.set_page_config(
-    page_title="Dashboard Analisis Sentimen Apex Legends di Steam"
-                "Dashboard Analisis Sentimen Apex Legends di Steam"
+    page_title="Dashboard Analisis Sentimen Apex Legends",
     page_icon="🎮",
     layout="wide",
     initial_sidebar_state="expanded",
